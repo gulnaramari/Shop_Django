6f2d7a979d5fea3f34eef80d9bc8ff5d00b3af23
Daniel <gulnarikmihailova@gmail.com>
2025-05-23 23:59:03 +0300

    создание модели продукт

--- myproject/catalog/models.py
+++ myproject/catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -24,4 +25,65 @@ class Category(models.Model):
     class Meta:
         verbose_name = "категория"
         verbose_name_plural = "категории"
+        ordering = ["name", "description"]
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Название продукта",
+        help_text="Введите название продукта",
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+    )
+
+    image = models.ImageField(
+        upload_to="images/",
+        blank=True,
+        null=True,
+        verbose_name="Фотография",
+        help_text="Загрузите фотографию продукта",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name="Название категории",
+        help_text="Введите название категории",
+        blank=True,
+        null=True,
+        related_name="products",
+    )
+
+    purchase_price = models.DecimalField(
+        blank=True,
+        default=0.0,
+        verbose_name="Цена",
+        help_text="Введите цену продукта",
+        decimal_places=2,
+        max_digits=100
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name="Дата создания",
+        help_text="Введите дату создания",
+        default=timezone.now
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Дата изменения",
+        help_text="Введите дату изменения",
+        default=timezone.now
+    )
+
+    def __str__(self):
+        return f"""
+        name: {self.name}, description: {self.description}
+                """
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
         ordering = ["name", "description"]
