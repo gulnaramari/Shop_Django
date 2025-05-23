ea9fbac6afb26c77aaf1382acbad8c34359c516f
Daniel <gulnarikmihailova@gmail.com>
2025-05-23 23:20:29 +0300

    создание модели категория

--- myproject/catalog/models.py
+++ myproject/catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Название категории",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+    )
+    category_updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Последняя модификация"
+    )
+
+    def __str__(self):
+        return f"name: {self.name}, description: {self.description}"
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = ["name", "description"]
