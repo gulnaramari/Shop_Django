c565d2c356b20558b34f19eab1b7b67a57de73b8
Daniel <gulnarikmihailova@gmail.com>
2025-05-24 00:28:20 +0300

    шаблонизация

---
+++ myproject/catalog/urls.py
@@ -0,0 +1,13 @@
+from .apps import CatalogConfig
+from django.urls import path
+from . import views
+
+
+app_name = CatalogConfig.name
+
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
+
+]
